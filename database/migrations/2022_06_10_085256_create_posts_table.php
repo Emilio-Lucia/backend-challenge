4bf44b84c705d8d56889e77478c03fa5637e88c7
Sergio Almela <sergio.almela@horizm.com>
2022-07-18 07:35:52 +0000

    Fix createPosts migration

--- database/migrations/2022_06_10_085256_create_posts_table.php
+++ database/migrations/2022_06_10_085256_create_posts_table.php
@@ -1,44 +1,39 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
-class User extends Authenticatable
+class CreatePostsTable extends Migration
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title', 150);
+            $table->string('body', 250);
+            $table->unsignedSmallInteger('rating');
+
+            $table->timestamps();
+
+            $table->index('user_id');
+        });
+    }
 
     /**
-     * The attributes that are mass assignable.
+     * Reverse the migrations.
      *
-     * @var array<int, string>
+     * @return void
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
 }
