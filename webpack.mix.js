4dbf1db168a44f83d3a7b160ce7e2db18e279ced
SRVonline <100998755+SRVonline@users.noreply.github.com>
2022-09-09 01:03:03 +0200

    Añadido soporte para Less.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    // .postCss('resources/css/less/app.less', 'public/css', [
+    //     //
+    // ])
+    .less( 'resources/css/less/app.less', 'public/css' )
+    .options( {
+        processCssUrls: false
+    } );
