1c4b110a6aa551aeb9a7f193663257c0a30be983
SRVonline <100998755+SRVonline@users.noreply.github.com>
2022-09-09 04:30:18 +0200

    Post: modelo y controlador.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\RemotePostsService;
+use Illuminate\Support\Facades\View;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -32,9 +35,54 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store( Request $request, $amount = 50, RemotePostsService $rpService )
     {
-        //
+        $remote_posts = $rpService->getPosts( $amount );
+
+
+        $cont = 0;
+
+        foreach ( $remote_posts as $remote_post ) {
+
+            $post = Post::find( $remote_post[ 'id' ] );
+
+            if ( empty( $post ) ) {
+
+                $post           = Post::create( [
+                    'id'        => $remote_post[ 'id' ],
+                    'user_id'   => $remote_post[ 'userId' ],
+                    'title'     => $remote_post[ 'title' ],
+                    'body'      => $remote_post[ 'body' ],
+                    'rating'    => \call_user_func( function( $remote_post ) {
+
+                        $rating = str_word_count( $remote_post[ 'title' ] ) * 2;
+                        $rating += str_word_count( $remote_post[ 'body' ] );
+
+                        return $rating;
+
+                    }, $remote_post )
+                ] );
+
+            }
+            else {
+
+                $post->body = $remote_post[ 'body' ];
+
+            }
+
+            $success = $post->save();
+
+            $success && ( $cont++ );
+
+        }
+
+        return View::make( 'getposts', [
+
+            // 'posts'     => $remote_posts
+            'cont'  => $cont
+
+        ] );
+
     }
 
     /**
